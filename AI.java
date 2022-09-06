1ded8ba4ba1369b12f5fefad54d15e66b32cc9eb
Kreseph (KJ) <94188300+Kreseph@users.noreply.github.com>
2022-09-06 21:18:11 +0000

    Initial commit

---
+++ AI.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class AI{
+	public List<String> onebias;
+	public int total_sticks;
+	public int curr_sticks;
+	
+	public AI(int total_sticks, int curr_sticks){
+
+
+		
+		
+		onebias.add(0, "hi");
+		System.out.println(onebias);
+	}
+
+	
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+class Main {
+  public static void main(String[] args) {
+    AI ai = new AI(10, 0);
+  }
+}
